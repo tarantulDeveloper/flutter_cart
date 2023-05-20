f8a7220e215ce2b7601e23d4e46109068bdb7908
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-05-20 10:03:20 +0600

    first commit

---
+++ lib/cart/cart_bloc/cart_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'cart_bloc.dart';
+import 'cart_event.dart';
+import 'cart_state.dart';
+
+class CartPage extends StatefulWidget {
+  const CartPage({super.key});
+
+  @override
+  _CartPageState createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder(
+        bloc: BlocProvider.of<CartBloc>(context),
+        builder: (context, snapshot) {
+          if (snapshot is CartState) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("Your cart (${snapshot.cart.length})"),
+              ),
+              body: ListView(
+                children: snapshot.cart
+                    .map(
+                      (e) => ListTile(
+                        title: Text(e.name ?? ''),
+                        subtitle: Text("USD  ${e.price ?? ''}"),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.remove_circle),
+                          onPressed: () {
+                            BlocProvider.of<CartBloc>(context).add(RemoveItemFromCartEvent(item: e));
+                          },
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            );
+          }
+          return Container();
+        });
+  }
+}
---
+++ test/cart_event.dart
@@ -0,0 +1,15 @@
+import 'item.dart';
+
+class CartEvent {}
+
+class AddItemToCartEvent extends CartEvent {
+  final Item item;
+
+  AddItemToCartEvent({required this.item});
+}
+
+class RemoveItemFromCartEvent extends CartEvent {
+  final Item item;
+
+  RemoveItemFromCartEvent({required this.item});
+}
